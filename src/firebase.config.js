c57713b5fc1eda6790e01e757380afd8ca4a8c61
Shivam <151379988+Shivam0802@users.noreply.github.com>
2024-09-04 10:10:01 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey:"AIzaSyAV0h52gV1Ib6yXSr3GFaiDG_KjZLzLDFc",
   authDomain: "blogsphere-93d6d.firebaseapp.com",
   projectId: "blogsphere-93d6d",
   storageBucket: "blogsphere-93d6d.appspot.com",
